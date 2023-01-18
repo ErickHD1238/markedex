128509cfefbd225ad0bcd8da93a57237ff69b228
ErickHD1238 <erickhudel@gmail.com>
2023-01-17 23:10:28 -0400

    Se agrega el proyecto

---
+++ app/src/main/java/uni/erick/markedex/datos/tienda.java
@@ -0,0 +1,24 @@
+package uni.erick.markedex.datos;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+import uni.erick.markedex.extra.Extra;
+
+public class tienda {
+    /*public tienda() {
+        //Objetos de Volley
+        RequestQueue solicitud;
+        //Objeto de la clase con datos generales
+        Extra extra = new Extra();
+        //Vectores
+        String[] vNombre = new String[50];
+        String[] vIdTienda = new String[50];
+        String[] vLat = new String[50];
+        String[] vLon = new String[50];
+
+        int filasRecibidas = -1; //catnidad de casillas con datos
+
+        solicitud = Volley.newRequestQueue(this);
+    }*/
+}
